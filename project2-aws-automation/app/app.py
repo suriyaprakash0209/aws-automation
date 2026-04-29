5a5fcc5e6b36cc93224296977c58ee52d3ea3d8d
suriyaprakash0209 <suriyaprakash.j@greatify.ai>
2026-04-29 18:06:33 +0530

    updated features

--- project2-aws-automation/app/app.py
+++ project2-aws-automation/app/app.py
@@ -1,11 +1,15 @@
-# app.py
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return {"status": "ok"}
+    return jsonify({"status": "ok"}), 200
+
+@app.route("/")
+def home():
+    return jsonify({"message": "AWS Automation Running"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
